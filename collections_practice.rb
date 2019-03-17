1dbceeeebfc08ddc8c12f1690e1436c4f58202c7
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 04:19:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,18 @@ def sort_array_desc(array)
     right <=> left
   end
 end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
